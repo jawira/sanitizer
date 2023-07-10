772abec736dcb10d0a2d313e741f90fd2fc73689
Jawira Portugal <dev@tugal.be>
2023-07-10 23:01:52 +0200

    feat: Create Digits filter

---
+++ src/Filters/Digits.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Jawira\Sanitizer\Filters;
+
+#[\Attribute]
+class Digits implements FilterInterface
+{
+  public function check(mixed $value): bool
+  {
+    return is_string($value);
+  }
+
+  public function filter(mixed $value): mixed
+  {
+    assert(is_string($value));
+
+    return preg_replace('#\D+#', '', $value);
+  }
+}
